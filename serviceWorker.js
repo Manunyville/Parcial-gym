954df6f3885665dc490938eae5533fd0eb49c14e
Manunyville <125371151+Manunyville@users.noreply.github.com>
2025-05-09 11:53:26 -0300

    ...

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,3 +1,10 @@
 window.addEventListener('DOMContentLoaded', function() {
-
+    if('serviceWorker' in navigator) 
+        {
+            navigator.serviceWorker.register('serviceWoker.js')
+            .then( register =>{
+                console.log(register);
+                
+            })
+        }
 });
